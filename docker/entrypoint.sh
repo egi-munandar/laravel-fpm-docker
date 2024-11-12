1a8764a2539c9ff5f25975f512ab09df7c6bfb58
egi-munandar <egi_munandar@outlook.com>
2024-11-12 15:15:21 +0700

    fix connection refused

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,6 @@
 chmod 0644 /etc/cron.d/cron&
 crontab /etc/cron.d/cron&
 touch /var/log/cron.log&
-cron -f &
-/usr/bin/supervisord -n -c /etc/supervisor/supervisor.conf
+sudo cron -f &
+/usr/bin/supervisord -n -c /etc/supervisor/supervisor.conf&
+docker-php-entrypoint php-fpm
